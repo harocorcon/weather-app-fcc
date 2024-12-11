63694462c29675c442882d547b17e3ac18aa712f
harocorcon <hracconsular@gmail.com>
2024-12-11 21:08:16 +0800

    adding first section 62:07

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,79 @@
-import Image from "next/image";
+'use client';
 import Navbar from "./components/Navbar";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { format, parseISO } from "date-fns";
+import Container from "./components/Container";
+import { convertKelvinToCelcius } from "@/utils/convertKelvinToCelcius";
+
 export default function Home() {
+  const apiKey = process.env.NEXT_PUBLIC_WEATHER_KEY;
+  console.log("api ", apiKey);
+  const { isLoading, error, data } = useQuery<WeatherData>(
+    // {queryKey: ['repoData'], queryFn: async () => {
+    "repoData", async() => {
+      const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=hindang&appid=${apiKey}&cnt=56`);
+      return data;
+    });
+    
+  const firstData = data?.list[0];
+  console.log("data: ", data?.city.country);
+  if (isLoading) 
+    return (
+    <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+    </div>
+  );
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar/>
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* data today */}
+        <section className="space-y-4">
+          <div className="space-y-2"> 
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p> {format(parseISO(firstData?.dt_txt ?? ""), "EEEE")} </p>
+              <p className="text-lg ">{format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")}</p>
+            </h2>
+            <Container className="gap-10 px-6 items-center">
+              {/* temperature left */}
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToCelcius(firstData?.main.temp ?? 296.37)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    {convertKelvinToCelcius(firstData?.main.temp_min ?? 0)}°↓{" "}
+                  </span>
+                  <span>
+                  {" "}{convertKelvinToCelcius(firstData?.main.temp_max ?? 0)}°↑
+                  </span>
+                </p>
+              </div>
+              {/* time and weather icon */}
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((d, i)=>
+                <div
+                  key={i}
+                  className="flex flex-col justify-between gap-2 items-center text-xs font-semibold">
+                    <p className="whitespace-nowrap">
+                      {format(parseISO(d.dt_txt), "h:mm a")}
+                    </p>
+                    <p>{convertKelvinToCelcius(d?.main.temp ?? 0)}°</p>
+
+                </div>
+                )}
+              </div>
+            </Container>
+          </div>
+        </section>
+        {/* 7 day data */}
+        <section></section>
+      </main>
     </div>
   );
 }
